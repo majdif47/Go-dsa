53d70c331d0c0a8dfc79198c740b6b4d5accf74b
Majdi Fahoum <majdif47@gmail.com>
2025-01-05 20:04:59 +0200

    Inital Commit: Go DSA Library

---
+++ ds/stacks/stacks.go
@@ -0,0 +1,54 @@
+
+
+package stacks
+
+
+import "errors"
+
+
+type Stack struct {
+  elements []int
+}
+
+func NewStack() *Stack {
+  return &Stack{
+    elements: []int{},
+  }
+}
+
+
+
+func (s *Stack) Push(x int) {
+  s.elements = append(s.elements, x)
+}
+
+func (s *Stack) Pop() (int, error) {
+  if s.isEmpty() {
+    return 0, errors.New("Stack is empty")
+  }
+  top := s.elements[len(s.elements)-1]
+  s.elements = s.elements[:len(s.elements)-1]
+  return top, nil
+}
+
+func (s *Stack) Size() int {
+  return len(s.elements)
+}
+
+func (s *Stack) Peek() (int, error) {
+  if s.isEmpty() {
+    return 0, errors.New("Stack is empty")
+  }
+  return s.elements[len(s.elements)-1], nil
+}
+
+func (s *Stack) isEmpty() bool {
+  return len(s.elements) == 0
+}
+
+func (s *Stack) Empty() {
+  s.elements = nil
+}
+
+
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/majdif47/Go-dsa
+
+go 1.23.4
